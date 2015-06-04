8c95f577065b86e3b75449148ff3b4691cb44b81
Guilherme Lourenço <Guilherme Loureno@192.168.25.3>
2015-06-04 16:42:56 -0300

    Classe que inicia o projeto.

---
+++ Projeto-Taiti/src/br/com/projeto/view/InicioProjeto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto.view;
+
+/**
+ *
+ * @author Guilherme Lourenço
+ */
+public class InicioProjeto {
+    public static void main(String[] args) {
+        TelaPrincipal novaTela = new TelaPrincipal();
+        novaTela.setVisible(true);
+    }
+}
